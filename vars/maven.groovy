142cfdbef5cf4c214d0c703048c01b860ad16b28
vMahidhar <53318668+vMahidhar@users.noreply.github.com>
2019-08-08 17:38:01 -0400

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -11,7 +11,16 @@ def call(maven) {
           }
           stage('echo') { 
                 echo 'cloned project' 
-          }   
+          }
+              withMaven(
+        // Maven installation declared in the Jenkins "Global Tool Configuration"
+        maven: 'maven',
+        // Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+        // We recommend to define Maven settings.xml globally at the folder level using
+        // navigating to the folder configuration in the section "Pipeline Maven Configuration / Override global Maven configuration"
+        // or globally to the entire master navigating to  "Manage Jenkins / Global Tools Configuration"
+        mavenSettingsConfig: 'my-maven-settings') 
+    
           stage('clean') { 
                 sh 'mvn clean -f pipeline-library-demo' 
           }
