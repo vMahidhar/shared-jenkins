3ecf7e41632b34d7fc3381d804b98748d135ff0b
vMahidhar <53318668+vMahidhar@users.noreply.github.com>
2019-08-08 16:47:44 -0400

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -4,22 +4,14 @@ def call(maven) {
   
   pipeline {
     
-      stages {
           stage('clean') { 
-               steps {
                 sh 'mvn clean' 
-              }
           }
           stage('validate') { 
-              steps {
                   sh 'mvn validate' 
-              }
           }
           stage('Deploy') { 
-              steps {
                   sh 'mvn package'
-              }
           }
-      }
     }
 }
