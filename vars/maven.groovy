f5dff539f5bdf24cc2bf47e99c8c35c6d84f2c8e
vMahidhar <53318668+vMahidhar@users.noreply.github.com>
2019-08-09 12:03:34 -0400

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -3,23 +3,15 @@
 def call(maven) {
   
   node {
-          stage('clean previous repo') { 
-                sh 'rm -rf pipeline-library-demo' 
-          }
-          stage('git clone') { 
-                sh 'git clone https://github.com/vMahidhar/pipeline-library-demo.git' 
-          }
-          stage('echo') { 
-                echo 'cloned project' 
-          }    
+       
           stage('clean') { 
-                sh 'mvn clean -f pipeline-library-demo' 
+                sh 'mvn clean' 
           }
           stage('validate') { 
-                  sh 'mvn validate -f pipeline-library-demo' 
+                  sh 'mvn validate' 
           }
           stage('Deploy') { 
-                  sh 'mvn package -f pipeline-library-demo'
+                  sh 'mvn package'
           }
     }
 }
